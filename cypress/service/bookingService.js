ce92cdafbad6c266fa22154454cca0ee2ec42c31
Rodrigo Caetano dos Santos <rodrigo@MacBook-Air-de-Anna.local>
2026-03-24 08:10:24 -0300

    ajustado service e add schemas

--- cypress/service/bookingService.js
+++ cypress/service/bookingService.js
@@ -1,42 +1,61 @@
 class BookingService {
 
-  static criarReserva(data) {
+  static createBooking(payload) {
+
     return cy.request({
       method: 'POST',
-      url: `${Cypress.env('apiUrlrestful')}/booking`,
-      body: data,
+      url: `${Cypress.env('apiUrl')}/booking`,
+      body: payload,
       failOnStatusCode: false
     })
+
   }
 
-  static listarReservas() {
+  static getBooking(id) {
+
     return cy.request({
       method: 'GET',
-      url: `${Cypress.env('apiUrlrestful')}/booking`
+      url: `${Cypress.env('apiUrl')}/booking/${id}`,
+      failOnStatusCode: false
     })
+
   }
 
-  static gerarToken() {
+  static getAllBookings() {
+
+    return cy.request({
+      method: 'GET',
+      url: `${Cypress.env('apiUrl')}/booking`
+    })
+
+  }
+
+  static generateToken() {
+
     return cy.request({
       method: 'POST',
-      url: `${Cypress.env('apiUrlrestful')}/auth`,
+      url: `${Cypress.env('apiUrl')}/auth`,
       body: {
-        username: 'admin',
-        password: 'password123'
+        username: "admin",
+        password: "password123"
       }
     }).then(res => res.body.token)
+
   }
 
-  static deletarReserva(id, token) {
+  static deleteBooking(id, token) {
+
     return cy.request({
       method: 'DELETE',
-      url: `${Cypress.env('apiUrlrestful')}/booking/${id}`,
+      url: `${Cypress.env('apiUrl')}/booking/${id}`,
       headers: {
         Cookie: `token=${token}`
       },
       failOnStatusCode: false
     })
+
   }
+
 }
 
 export default BookingService
